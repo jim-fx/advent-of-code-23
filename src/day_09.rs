382f477262f42c2b6210dc5a7663bbe5446cbdbf
Max Richter <jim-x@web.de>
2023-12-09 18:44:20 +0100

    feat: solve day 9 part 2

--- src/day_09.rs
+++ src/day_09.rs
@@ -3,22 +3,24 @@ fn solve_line(line: Vec<i64>) -> i64 {
     current_lines.push(line.clone());
     let mut line_index = 0;
     let length = line.len();
-    while line_index < length - 1 {
+    while length > 0 {
         let current_line = current_lines.get(line_index).unwrap();
         let mut new_line = current_line.clone();
         new_line.fill(0);
 
-        let mut line_total = 0;
+        let mut finished = true;
 
         // start at the second index in the line
         // _ i 2 3 4 5 6 7 8 9 10
-        for i in (line_index + 1)..length {
+        for i in 1..(length - line_index) {
             let diff = current_line[i] - current_line[i - 1];
-            line_total += diff;
-            new_line[i] = diff;
+            if diff != 0 {
+                finished = false;
+            }
+            new_line[i - 1] = diff;
         }
 
-        if line_total == 0 {
+        if finished {
             break;
         }
 
@@ -26,10 +28,10 @@ fn solve_line(line: Vec<i64>) -> i64 {
         line_index += 1;
     }
 
-    let mut end_num = *current_lines.last().unwrap().last().unwrap();
+    let mut end_num = current_lines[line_index][0];
     for _i in 1..(line_index + 1) {
         let i = line_index - _i;
-        end_num += current_lines[i][length - 1];
+        end_num = current_lines[i][0] - end_num;
     }
 
     return end_num;
@@ -49,8 +51,10 @@ pub fn solve(input: String) -> (u64, u64) {
 
     for line in lines {
         let res = solve_line(line);
+        println!("{}", res);
         total += res;
     }
+
     println!("{}", total);
     return (0, 0);
 }
